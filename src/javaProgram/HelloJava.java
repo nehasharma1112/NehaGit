a69dd3ab6dfa014f337bda893785f8c21bd298b0
nehasharma <neha.yadav.2009@gmail.com>
2019-03-29 12:53:58 +0530

    message

---
+++ src/javaProgram/HelloJava.java
@@ -0,0 +1,14 @@
+package javaProgram;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i>=5;i++);
+		{
+			System.out.println("hello world");
+		}
+
+	}
+
+}
